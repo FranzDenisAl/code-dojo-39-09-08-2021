4bf5edfaaba3cdb8ee38f54f2d953ec54f87cbd0
FranzDenisAl <ivermiyata19@gmail.com>
2021-10-21 16:30:37 -0400

    Con Un animal

--- python/song.py
+++ python/song.py
@@ -1,5 +1,5 @@
 ORIGINAL_ANIMALS_OF_THE_SONG = ['fly', 'spider', 'bird', 'cat', 'dog', 'cow', 'horse']
-
+UN_ANIMAL = 1
 
 class Song:
     first_verse_of_the_song = "There was an old lady who swallowed a {}."
@@ -22,10 +22,13 @@ class Song:
         else:
             self.animals_for_song = animals_for_song
 
+
+
     def adapt_original_lyrics(self):
-        if len(self.animals_for_song) == 1:
-            final_verse_of_the_song = self.final_verse_of_the_song.format(self.animals_for_song[0])
-            return final_verse_of_the_song
+
+
+        if len(self.animals_for_song) == UN_ANIMAL:
+            return self.adaptar_cancion_para_un_solo_animal()
         else:
             amount_of_animals = len(self.animals_for_song)
             final_song = ""
@@ -63,6 +66,10 @@ class Song:
                     else:
                         funny_verse_position += 1
             return final_song
+
+    def adaptar_cancion_para_un_solo_animal(self):
+        final_verse_of_the_song = self.final_verse_of_the_song.format(self.animals_for_song[0])
+        return final_verse_of_the_song
 
 
 class Singer:
